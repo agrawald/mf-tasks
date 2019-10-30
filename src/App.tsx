7c987c9d53fd2e0147398bd66c4c83b15c19abb0
Dheeraj Agrawal <dheeraj.agrawal@industrieit.com>
2019-10-30 21:42:45 +1100

    Fixing the routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import * as React from 'react';
 import Task from './components/Task';
 
 class App extends React.Component {
+  componentDidCatch(error: any, info: any) {
+    console.error('Tasks Micro-app: ' + info, error);
+  }
   public render() {
     return <Task />;
   }
